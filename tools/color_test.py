b5e43bada760c2c1e25ade5902dcad1e614dce7f
fm3chanic <f.m3chanic@gmail.com>
2025-08-14 19:24:16 +0200

    adding color testing code

---
+++ tools/color_test.py
@@ -0,0 +1,44 @@
+def main():
+    print("\n")
+    for i in range(2):
+        print(
+            "\x1b[40m    \x1b[0m" +
+            "\x1b[41m    \x1b[0m" +
+            "\x1b[42m    \x1b[0m" +
+            "\x1b[43m    \x1b[0m" +
+            "\x1b[44m    \x1b[0m" +
+            "\x1b[45m    \x1b[0m" +
+            "\x1b[46m    \x1b[0m" +
+            "\x1b[47m    \x1b[0m"
+        )
+    for i in range(2):
+        print(
+            "\x1b[100m    \x1b[0m" +
+            "\x1b[101m    \x1b[0m" +
+            "\x1b[102m    \x1b[0m" +
+            "\x1b[103m    \x1b[0m" +
+            "\x1b[104m    \x1b[0m" +
+            "\x1b[105m    \x1b[0m" +
+            "\x1b[106m    \x1b[0m" +
+            "\x1b[107m    \x1b[0m"
+        )
+    print("\n")
+    print(
+            "\x1b[30m BG2   \x1b[0m" + "\x1b[90m HL3 \x1b[0m" + "\n" +
+            "\x1b[31m MAIN1 \x1b[0m" + "\x1b[91m SUB1 \x1b[0m" + "\n" +
+            "\x1b[32m MAIN6 \x1b[0m" + "\x1b[92m SUB6 \x1b[0m" + "\n" +
+            "\x1b[33m MAIN5 \x1b[0m" + "\x1b[93m SUB5 \x1b[0m" + "\n" +
+            "\x1b[34m MAIN3 \x1b[0m" + "\x1b[94m SUB3 \x1b[0m" + "\n" +
+            "\x1b[35m MAIN2 \x1b[0m" + "\x1b[95m SUB2 \x1b[0m" + "\n" +
+            "\x1b[36m MAIN4 \x1b[0m" + "\x1b[96m SUB4 \x1b[0m" + "\n" +
+            "\x1b[37m FG1   \x1b[0m" + "\x1b[97m FG1 \x1b[0m"
+    )
+    print("\n")
+    print("   _____                 . . . . . o o o o o")
+    print("  __|[_]|__ ___________ _______    ____      o")
+    print(" |[] [] []| [] [] [] [] [_____(__  ][]]_n_n__][.")
+    print("_|________|_[_________]_[________]_|__|________)<")
+    print("  oo    oo \'oo      oo \' oo    oo \'oo 0000---oo\\_") # \\ = \ & \' = '
+    print(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    
+main()

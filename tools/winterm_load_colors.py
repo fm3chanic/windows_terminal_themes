c86e4af049a678bf13be067edc68e769040dad53
fm3chanic <f.m3chanic@gmail.com>
2025-11-01 16:16:46 +0100

    updating python scripts

--- tools/winterm_load_colors.py
+++ tools/winterm_load_colors.py
@@ -27,7 +27,7 @@ def create_files(tab1_replace, tab2_replace, tab1_values, tab2_values, templates
             content = content.replace(tab1_replace[i], tab1_values[i])
 
     if index == 0:
-        for i in range(7):
+        for i in range(6):
             content = content.replace(tab2_replace[i], tab2_values[i])    
 
     if index != 0 and int(sys.argv[2]) == 1:
@@ -50,7 +50,7 @@ tab2_values = []
 
 #define values for replacement
 tab1_replace = ['BackGround1','BackGround2','BackGround3','ForeGround1','ForeGround2','ForeGround3','HighLight1','HighLight2','HighLight3']
-tab2_replace = ['Syn1','Syn2','Syn3','Syn4','Syn5','Syn6','Syn7']
+tab2_replace = ['Syn1','Syn2','Syn3','Syn4','Syn5','Syn6']
 
 #define values for file creation
 input_file = f'{sys.argv[1]}.html'
